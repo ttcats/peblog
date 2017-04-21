8df72b62e86b2f69ce27b1cd0e9bac0860843db2
ttcats <ttcats@sina.com>
2017-04-21 19:10:33 +0800

    js bug

--- peblog/blog/views.py
+++ peblog/blog/views.py
@@ -1,5 +1,5 @@
-#coding=utf-8
-from django.shortcuts import render,get_object_or_404
+# coding=utf-8
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
@@ -10,90 +10,98 @@ from collections import Counter
 
 # 按时间归档
 def archives_info():
-    alltimes = [ blog.timestamp.strftime('%Y-%m') for blog in Blog.objects.all() ]
+    alltimes = [blog.timestamp.strftime('%Y-%m')
+                for blog in Blog.objects.all()]
     f_archives = dict(Counter(alltimes))
-    sort_archives = [ (key,f_archives[key]) for key in sorted(f_archives.keys()) ]
+    sort_archives = [(key, f_archives[key])
+                     for key in sorted(f_archives.keys())]
     sort_archives.reverse()
     archives = dict(sort_archives[:12])
     return archives
 
 
 # 分页
-def pages_info(page_id,allblogs):
+def pages_info(page_id, allblogs):
     articlenumber = settings.ARTICLE_NUMBER
-    page_id = (page_id,None)[int(page_id)==0]
-    if page_id == None and len(allblogs) <= int(articlenumber):
+    page_id = (page_id, None)[int(page_id) == 0]
+    if page_id is None and len(allblogs) <= int(articlenumber):
         blogs = allblogs
         nextpage_id = None
-    elif page_id == None and len(allblogs) > int(articlenumber):
+    elif page_id is None and len(allblogs) > int(articlenumber):
         blogs = allblogs[:int(articlenumber)]
         nextpage_id = 1
     else:
         last_id = int(page_id) * int(articlenumber)
-        next_id = ( int(page_id) + 1 ) * int(articlenumber)
+        next_id = (int(page_id) + 1) * int(articlenumber)
         blogs = allblogs[last_id:next_id]
         nextpage_id = int(page_id) + 1
         if next_id >= len(allblogs):
             nextpage_id = None
-    return [page_id,nextpage_id,blogs]
-
+    return [page_id, nextpage_id, blogs]
 
 
 def index(request):
-    page_id = request.GET.get('page',0)
+    page_id = request.GET.get('page', 0)
     allblogs = Blog.objects.order_by('-timestamp')
 
-    [page_id,nextpage_id,blogs] = pages_info(page_id,allblogs)
+    [page_id, nextpage_id, blogs] = pages_info(page_id, allblogs)
     archives = archives_info()
-    return render(request, 'index.html',locals())
+    return render(request, 'index.html', locals())
 
 
-def blog(request,number):
+def blog(request, number):
     archives = archives_info()
     blog_id = int(number) - 1000
     try:
         articles = Blog.objects.get(id=blog_id)
-        last_blog = Blog.objects.filter(timestamp__lt=articles.timestamp).order_by('timestamp').last()
-        next_blog = Blog.objects.filter(timestamp__gt=articles.timestamp).order_by('timestamp').first()
-        print(last_blog,next_blog)
+        last_blog = Blog.objects.filter(
+            timestamp__lt=articles.timestamp).order_by('timestamp').last()
+        next_blog = Blog.objects.filter(
+            timestamp__gt=articles.timestamp).order_by('timestamp').first()
+        print(last_blog, next_blog)
     except Exception as e:
         print(e)
-    return render(request, 'blog.html',locals())
+    return render(request, 'blog.html', locals())
 
 
-def archive(request,times):
-    page_id = request.GET.get('page',0)
-    archives = archives_info()
-    
-    allblogs = [ blog for blog in Blog.objects.order_by('-timestamp') if blog.timestamp.strftime('%Y-%m') == times ]
-    print(pages_info(page_id,allblogs))
-    [page_id,nextpage_id,blogs] = pages_info(page_id,allblogs)
-    return render(request, 'archives.html',locals())
-
-
-def tag(request,tag):
-    page_id = request.GET.get('page',0)
+def archive(request, times):
+    page_id = request.GET.get('page', 0)
     archives = archives_info()
 
-    allblogs = Blog.objects.filter(tag=BlogTag.objects.get(tag=tag).id).order_by('-timestamp')
-    [page_id,nextpage_id,blogs] = pages_info(page_id,allblogs)
-    return render(request, 'tags.html',locals())
+    allblogs = [blog for blog in Blog.objects.order_by(
+        '-timestamp') if blog.timestamp.strftime('%Y-%m') == times]
+    print(pages_info(page_id, allblogs))
+    [page_id, nextpage_id, blogs] = pages_info(page_id, allblogs)
+    return render(request, 'archives.html', locals())
+
+
+def tag(request, tag):
+    page_id = request.GET.get('page', 0)
+    archives = archives_info()
+
+    allblogs = Blog.objects.filter(
+        tag=BlogTag.objects.get(
+            tag=tag).id).order_by('-timestamp')
+    [page_id, nextpage_id, blogs] = pages_info(page_id, allblogs)
+    return render(request, 'tags.html', locals())
+
 
 def search(request):
-    #print(request.GET)
+    # print(request.GET)
     archives = archives_info()
-    page_id = request.GET.get('page',0)
-    search_info = request.GET.get('search','')
+    page_id = request.GET.get('page', 0)
+    search_info = request.GET.get('search', '')
 
     allblogs = Blog.objects.filter(title__icontains=search_info)
-    [page_id,nextpage_id,blogs] = pages_info(page_id,allblogs)
-    return render(request, 'search.html',locals())
+    [page_id, nextpage_id, blogs] = pages_info(page_id, allblogs)
+    return render(request, 'search.html', locals())
+
 
 @csrf_exempt
-def comment(request,blogid):
+def comment(request, blogid):
     blog_id = int(blogid) - 1000
     title = Blog.objects.get(id=blog_id)
-    comment,critic = request.POST.get('comment'),request.POST.get('critic')
-    #print(comment,critic,title)
-    BlogComment.objects.create(title=title,intro=comment,randomname=critic)
+    comment, critic = request.POST.get('comment'), request.POST.get('critic')
+    # print(comment,critic,title)
+    BlogComment.objects.create(title=title, intro=comment, randomname=critic)
     return HttpResponse("ok")
